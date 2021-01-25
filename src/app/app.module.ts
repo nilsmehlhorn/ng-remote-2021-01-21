10651453ea084ec36da3de3b67ff1727274b401b
mehlhorn <mail@nils-mehlhorn.de>
2021-01-25 11:57:43 +0100

    Styling & Script

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { BookCardComponent } from './book/book-card/book-card.component';
-import { BookFilterPipe } from './book/pipes/book-filter.pipe';
 import { BookModule } from './book/book.module';
+
 
 @NgModule({
   declarations: [
